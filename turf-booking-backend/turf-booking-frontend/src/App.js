f38c6617152e7a72e3f859b81a324bae7b24fd7c
Saiashishm7 <saiashishm1508@gmail.com>
2025-01-28 09:06:03 +0530

    commit

--- turf-booking-backend/turf-booking-frontend/src/App.js
+++ turf-booking-backend/turf-booking-frontend/src/App.js
@@ -5,10 +5,12 @@ import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import AdminDashboard from './pages/AdminDashboard';
 import Homepage from './pages/Homepage';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar /> {/* Single cohesive Navbar */}
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/signup" element={<SignUp />} />
---
+++ turf-booking-backend/turf-booking-frontend/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <header className="navbar">
+      <div className="site-name">kickNclick</div>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/dashboard">Dashboard</Link></li>
+          <li><Link to="/admin">Admin</Link></li>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/signup">Sign Up</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
